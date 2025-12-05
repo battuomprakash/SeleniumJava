2f8ede0020feb18c447f449c985256eeaa39dc7c
battuomprakash <batuomprakash@gmail.com>
2025-12-05 19:40:05 +0530

    Basic maven framework

---
+++ src/test/java/techlearn/Login.java
@@ -0,0 +1,35 @@
+package techlearn;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class Login {
+	WebDriver driver;
+  @Test
+  public void TechlearnLogin() {
+	  driver.get("https://www.techlearn.in/admin");
+	  
+  }
+  
+  @Test
+  public void Google() {
+	  driver.get("https://www.google.com");
+	  
+  }
+  
+  @Test
+  public void Facebook() {
+	  driver.get("https://www.facebook.com");
+	  
+  }
+
+  
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+  }
+
+}
