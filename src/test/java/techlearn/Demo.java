81f9e026f7545e27ec78e313bb3b9fe05b2a07a9
battuomprakash <batuomprakash@gmail.com>
2026-03-05 13:14:41 +0530

    update

--- src/test/java/techlearn/Demo.java
+++ src/test/java/techlearn/Demo.java
@@ -1,14 +1,18 @@
 package techlearn;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
 
 public class Demo {
+	
+	WebDriver driver;
   @Test
   public void f() {
   }
   @BeforeTest
   public void beforeTest() {
+	  
   }
 
 }
